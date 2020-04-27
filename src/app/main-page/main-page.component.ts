22255905cddb9c8695e3b3bf71dff1ec0cd56ae7
Luat Nguyen <761613@ACDM.DS.SAIT.CA>
2020-04-27 01:28:20 -0600

    updated version

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -6,34 +6,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
-  providers:[Service]
+  providers: [Service],
 })
 export class MainPageComponent implements OnInit {
   result;
-
+  location: any;
 
   countryForm: FormGroup;
   displayInfo: boolean;
-  constructor(private formBuilder: FormBuilder,private service:Service) { 
+  error: boolean;
+  constructor(private formBuilder: FormBuilder, private service: Service) {
     this.countryForm = this.formBuilder.group({
-      'country': ['', Validators.required]
-    })
+      country: ['', Validators.required],
+    });
   }
 
   ngOnInit(): void {
     this.displayInfo = false;
+    this.service.getLocation().subscribe(
+      (val) => {
+        this.location = val;
+        this.onLoad(this.location.country_name);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  onSubmit(){
+  onLoad(country: string) {
+    console.log('here:' + this.location);
+    this.service.getCorona(country).subscribe(
+      (da) => (this.result = da),
+      (error) => console.log(error),
+      () => console.log('complete')
+    );
+  }
+
+  onSubmit() {
     let country = Object.values(this.countryForm.value);
     console.log(this.countryForm.value);
     let c: any = country[0];
     console.log(c);
-    this.service.getCorona(c).subscribe(da => this.result = da);
-    this.displayInfo = true;
+    this.service.getCorona(c).subscribe(
+      (da) => (this.result = da),
+      (error) => console.log(error),
+      () => console.log('complete')
+    );
     this.countryForm.reset();
   }
-
 }
 
 // $('.timer').each(function () {
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 
 
@@ -9,22 +9,30 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class Service {
-  urls:string = 'https://covid-193.p.rapidapi.com/statistics?country=';
+  urls: string = 'https://covid-193.p.rapidapi.com/statistics?country=';
   key: string = 'e6381b6c46msh7328f7eee0c8bbcp166579jsn086e8cdae269';
   host: string = 'covid-193.p.rapidapi.com';
   result: Object;
 
+  keyLocation = 'bcef465c436e4bf591215b84e8c11aa0';
+  urlLocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=';
+
+  location: Object;
 
   constructor(private http: HttpClient) { }
 
-  getCorona(country:string):Observable<Object>{
+  getCorona(country: string): Observable<Object> {
     console.log(country);
-    return this.http.get(this.urls + country,{
+    return this.http.get(this.urls + country, {
       "headers": {
         "x-rapidapi-host": this.host,
         "x-rapidapi-key": this.key
       }
-    }).pipe(tap(ressult => this.result = ressult));
+    }).pipe(map(ressult => this.result = ressult));
+  }
+
+  getLocation() {
+    return this.http.get(this.urlLocation + this.keyLocation).pipe(map(location => this.location = location));
   }
 }
 
